7f1b91422206eef285ff9bfdd8fdbc872cb7f5f7
Ratomir Vukadin <ratomir@live.com>
2024-01-12 10:03:08 +0100

    new api

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,7 +3,7 @@ import { commonEnv } from "./environment.common";
 const env: Partial<typeof commonEnv> = {
   production: true,
   environmentName: "production",
-  api_url: 'https://localhost:7232/'
+  api_url: 'https://w8-be.rvs.cloudns.biz/'
 };
 
 export const environment = { ...commonEnv, ...env };
